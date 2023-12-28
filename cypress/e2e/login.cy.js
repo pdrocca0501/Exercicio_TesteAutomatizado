6df680aafbf488b2051a176d79cd0acfd1803464
Pedro <pdrocca1997@gmail.com>
2023-12-28 16:04:24 -0300

    Exercício Ebac refeito

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress"/>
+
+context('Funcionalidade Login', () =>{
+
+  beforeEach(() => {
+      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+  });
+
+  afterEach(() => {
+      cy.screenshot()
+  });
+
+  it('Deve fazer login com sucesso', () => {      
+    var login = 'aluno_ebac@teste.com'
+    var senha = 'teste@teste.com'
+    
+      cy.get('#username').type(login)
+      cy.get('#password').type(senha)
+      cy.get('.woocommerce-form > .button').click()
+
+      cy.get('.page-title').should('contain' , 'Minha conta')
+  })
+
+  it('Deve exibir uma mensagem de erro ao inserir usuario inválidos', () => {
+      cy.get('#username').type('ebac@teste.com')
+      cy.get('#password').type('teste@teste')
+      cy.get('.woocommerce-form > .button').click()
+
+      cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido.')
+  })
+
+  it('Deve exibir uma mensagem de erro ao inserir senha inválidas', () => {
+      cy.get('#username').type('aluno_ebac@teste.com')
+      cy.get('#password').type('teste@teste')
+      cy.get('.woocommerce-form > .button').click()
+
+      cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail')
+  })
+} )
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,31 @@
+/// <reference types ="cypress" />
+
+describe('Funcionalidade Página de produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+        //.first()
+        //.last()
+        //.eq(3)
+        .contains('Aero Daily Fitness Tee')
+        .click()
+    });
+    
+    it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 5
+
+        cy.get('[class="product-block grid"]')
+        .contains('Aero Daily Fitness Tee').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Yellow').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', '5 × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+
+    });
+});
