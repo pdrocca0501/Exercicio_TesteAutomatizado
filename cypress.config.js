3da32ae235ba2c24b6785909a91ff7b0d12512b5
Pedro <pdrocca1997@gmail.com>
2024-03-19 16:53:56 -0300

    mudança no git ignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ef4hiu',
   e2e: {baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video:true,
   },
 });
--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,31 +1,43 @@
 /// <reference types ="cypress" />
 
+import produtosPage from "../support/page-objects/produtos.page";
+
 describe('Funcionalidade Página de produtos', () => {
     
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('[class="product-block grid"]')
-        //.first()
-        //.last()
-        //.eq(3)
-        .contains('Aero Daily Fitness Tee')
-        .click()
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
     
-    it('Deve adicionar um produto ao carrinho', () => {
-        var quantidade = 5
-
-        cy.get('[class="product-block grid"]')
-        .contains('Aero Daily Fitness Tee').click()
-        cy.get('.button-variable-item-M').click()
-        cy.get('.button-variable-item-Yellow').click()
-        cy.get('.input-text').clear().type(quantidade)
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-        cy.get('.woocommerce-message').should('contain', '5 × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
-
+    
+    it('Deve buscar um produto com sucesso', () => {
+        let produto='Aether Gym Pant'
+        produtosPage.buscarProduto('Aether Gym Pant')
+        cy.get('.product_title').should('contain', 'Aether Gym Pant')
     });
+
+    it('Deve visitar a página de um produto', () => {
+        produtosPage.visitarProduto('Zeppelin-Yoga-Pant')
+    });
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        produtosPage.buscarProduto('Helena Hooded Fleece')
+        produtosPage.addProdutoCarrinho('S', 'Yellow', 1)
+        cy.get('.woocommerce-message').should('contain', '“Helena Hooded Fleece” foi adicionado no seu carrinho.')
+    });
+
+    it.only('Deve adicionar um produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho(dados[1].tamanho, dados[1].cor, dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+            
+        })
+        
+    });
+
 });
